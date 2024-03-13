fe49c12b18487d66d6dbb79a1224d34dcd4405b6
Ryan <rphillips0071@gmail.com>
2024-03-13 09:56:09 -0400

    Update configurations for GitHub Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import './App.css'
 import { Route, Routes } from "react-router-dom"
-import { BrowserRouter as Router } from 'react-router-dom';
+
 import CruiseSearch from './pages/CruiseSearch'
 import HomePage from './pages/HomePage'
 
 function App() {
   return (
-    <Router basename="/cruisewebsite">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/CruiseSearch" element={<CruiseSearch />} />
       </Routes>
-    </Router>
   );
 }
 
